2517f70c97f8fc66f665b5f0b66696cb4948202d
utknig123 <utkarshn04@gmail.com>
2025-11-06 19:19:54 +0530

    changes

--- backend/app.py
+++ backend/app.py
@@ -550,7 +550,7 @@ with app.app_context():
 
 # fix main guard
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
 
 
